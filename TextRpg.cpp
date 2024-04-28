742ed41b855d8a73ab40747c019f32bc518f6fbc
Badzoker <120088607+Badzoker@users.noreply.github.com>
2024-04-28 17:42:39 +0900

    textrpg 상점 마나 (스킬추가요망)

--- TextRpg.cpp
+++ TextRpg.cpp
@@ -15,12 +15,13 @@
 
 using namespace std;
 
-typedef struct tInfo
+typedef struct tInfo // 아마 소지금 및 보상도 계획해야할듯
 {
 	char szName[32];
 	int iHealth;
+	int iMana;
 	int iAttack;
-
+	
 }INFO;
 
 enum JOB
@@ -38,6 +39,9 @@ INFO* Choose_Char(int _iInput);
 void Print_Char(INFO* _player);
 void TextRpg_Home(INFO* _player);
 void TextRpg_Hunt(INFO* _player);
+void TextRpg_Market(INFO* _player);
+void TextRpg_Market_Equip(INFO* _player);
+void TextRpg_Market_Consumable(INFO* _player);
 void TextRpg_Fight(INFO* _player, INFO* _pMonster);
 void Create_Object(INFO** ppInfo, const char* _pName, int _iHealth, int _iAttack);
 void Print_Monster(INFO* _monster);
@@ -56,12 +60,12 @@ int main()
 void TextRpg_Menu()
 {
 	int iInput(0);
-	INFO* tPlayer = nullptr; 
+	INFO* tPlayer = nullptr;
 	cout << "직업을 선택하시오.(1. 전사 , 2. 마법사 , 3. 도적) :" << endl;
 	cin >> iInput;
 	switch (iInput)
 	{
-	case WARRIOR :
+	case WARRIOR:
 		tPlayer = Choose_Char(iInput);
 		break;
 	case WIZARD:
@@ -70,8 +74,8 @@ void TextRpg_Menu()
 	case THIEF:
 		tPlayer = Choose_Char(iInput);
 		break;
-	
-	default: 
+
+	default:
 		cout << "잘못 입력했습니다." << endl;
 		break;
 	}
@@ -89,16 +93,13 @@ INFO* Choose_Char(int _iInput)
 	switch (_iInput)
 	{
 	case 1:
-		tTemp = new INFO;
-		strcpy_s(tTemp->szName, 32, "전사");
-		tTemp->iHealth = 100;
-		tTemp->iAttack = 10;
+		Create_Object(&tTemp, "전사", 100, 20, 10);
 		break;
 	case 2:
-		Create_Object(&tTemp, "마법사", 50, 20);
+		Create_Object(&tTemp, "마법사", 50, 100,20);
 		break;
 	case 3:
-		Create_Object(&tTemp, "도적", 75, 15);
+		Create_Object(&tTemp, "도적", 75, 50,15);
 		break;
 	}
 	return tTemp;
@@ -107,7 +108,7 @@ INFO* Choose_Char(int _iInput)
 void Print_Char(INFO* _player)
 {
 	system("cls");
-	cout << "================\n직업 : " << _player->szName << "\n체력 : " << _player->iHealth << "\t  공격력 : " << _player->iAttack << endl;
+	cout << "================\n직업 : " << _player->szName << "\n체력 : " << _player->iHealth << "\n마나 : " << _player->iMana << "\t  공격력 : " << _player->iAttack << endl;
 }
 
 void TextRpg_Home(INFO* _player)
@@ -116,7 +117,7 @@ void TextRpg_Home(INFO* _player)
 	while (true)
 	{
 		Print_Char(_player);
-		cout << "1. 사냥터    2. 종료" << endl;
+		cout << "1. 사냥터    2. 상점    3. 상태창    4. 게임종료" << endl;
 		cin >> iInput;
 		switch (iInput)
 		{
@@ -124,6 +125,11 @@ void TextRpg_Home(INFO* _player)
 			TextRpg_Hunt(_player);
 			break;
 		case 2:
+			TextRpg_Market(_player);
+			break;
+		case 3:
+			return;
+		case 4:
 			return;
 		default:
 			cout << "잘못 입력했습니다." << endl;
@@ -139,22 +145,22 @@ void TextRpg_Hunt(INFO* _player)
 	while (true)
 	{
 		Print_Char(_player);
-		cout << "1. 초급\t2. 중급\t3. 고급\t4. 전 단계" << endl;
+		cout << "1. 초급\t2. 중급\t3. 고급\t4. 돌아가기" << endl;
 		cin >> iInput;
 		switch (iInput)
 		{
 		case 1:
-			Create_Object(&tMonster, "초급", 30 * iInput, 3 * iInput);
+			Create_Object(&tMonster, "초급", 30 * iInput, 10 * iInput,3 * iInput);
 			TextRpg_Fight(_player, tMonster);
 			break;
 		case 2:
-			Create_Object(&tMonster, "중급", 30 * iInput, 3 * iInput);
+			Create_Object(&tMonster, "중급", 30 * iInput, 10 * iInput, 3 * iInput);
 			TextRpg_Fight(_player, tMonster);
-				break;
+			break;
 		case 3:
-			Create_Object(&tMonster, "고급", 30 * iInput, 3 * iInput);
+			Create_Object(&tMonster, "고급", 30 * iInput, 10 * iInput, 3 * iInput);
 			TextRpg_Fight(_player, tMonster);
-				break;
+			break;
 		case 4:
 			/*if (tMonster != nullptr) // 질문 1. 없는데 지우려고 해서 그런건가?
 			{
@@ -169,6 +175,79 @@ void TextRpg_Hunt(INFO* _player)
 	}
 }
 
+void TextRpg_Market(INFO* _player)
+{
+	int iInput(0);
+	while (true)
+	{
+		Print_Char(_player);
+		cout << "1. 장비\t2. 도구\t3. 돌아가기" << endl;
+		cin >> iInput;
+		switch (iInput)
+		{
+		case 1:
+			TextRpg_Market_Equip(_player);
+			break;
+		case 2:
+			TextRpg_Market_Consumable(_player);
+			break;
+		case 3:
+			return;
+		default:
+			cout << "잘못 입력했습니다." << endl;
+			break;
+		}
+	}
+}
+void TextRpg_Market_Equip(INFO* _player)
+{
+	int iInput(0);
+	while (true)
+	{
+		Print_Char(_player);
+		if (strcmp(_player->szName, "전사"))
+		{
+			cout << "전사임"<< endl;
+		}
+		
+		cout << "1. 장비\t2. 도구\t3. 돌아가기" << endl;
+		cin >> iInput;
+		switch (iInput)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			return;
+		default:
+			cout << "잘못 입력했습니다." << endl;
+			break;
+		}
+	}
+}
+void TextRpg_Market_Consumable(INFO* _player)
+{
+	int iInput(0);
+	while (true)
+	{
+		Print_Char(_player);
+		cout << "1. 장비\t2. 도구\t3. 돌아가기" << endl;
+		cin >> iInput;
+		switch (iInput)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			return;
+		default:
+			cout << "잘못 입력했습니다." << endl;
+			break;
+		}
+	}
+}
 void TextRpg_Fight(INFO* _player, INFO* _pMonster)
 {
 	int iInput(0);
@@ -176,7 +255,7 @@ void TextRpg_Fight(INFO* _player, INFO* _pMonster)
 	{
 		Print_Char(_player);
 		Print_Monster(_pMonster);
-		cout << "1. 공격    2. 도망" << endl;
+		cout << "1. 공격    2. 스킬    3. 도구    4. 도망" << endl;
 		cin >> iInput;
 		switch (iInput)
 		{
@@ -185,6 +264,14 @@ void TextRpg_Fight(INFO* _player, INFO* _pMonster)
 			_pMonster->iHealth -= _player->iAttack;
 			break;
 		case 2:
+			delete _pMonster;
+			_pMonster = nullptr;
+			return;
+		case 3:
+			delete _pMonster;
+			_pMonster = nullptr;
+			return;
+		case 4:
 			delete _pMonster;
 			_pMonster = nullptr;
 			return;
@@ -217,17 +304,18 @@ void TextRpg_Fight(INFO* _player, INFO* _pMonster)
 	}
 }
 
-void Create_Object(INFO** ppInfo, const char* _pName, int _iHealth, int _iAttack)
+void Create_Object(INFO** ppInfo, const char* _pName, int _iHealth, int _iMana, int _iAttack)
 {
 	*ppInfo = new INFO;
 	strcpy_s((*ppInfo)->szName, sizeof(INFO), _pName);
 	(*ppInfo)->iHealth = _iHealth;
+	(*ppInfo)->iMana = _iMana;
 	(*ppInfo)->iAttack = _iAttack;
 }
 
 void Print_Monster(INFO* _pMonster)
 {
-	cout << "\n+++++++++++++\n괴물 : " << _pMonster->szName << "\n체력 : " << _pMonster->iHealth << "\t  공격력 : " << _pMonster->iAttack << endl;
+	cout << "\n+++++++++++++\n괴물 : " << _pMonster->szName << "\n체력 : " << _pMonster->iHealth << "\n마나 : " << _pMonster->iMana << "\t  공격력 : " << _pMonster->iAttack << endl;
 }
 
 #pragma endregion
